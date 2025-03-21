1c0e25306ac570e21d7793e73b3d6370bc31aa37
Mehrad Milanloo <m.milanoo@bitpin.ir>
2025-03-21 22:03:26 +0330

    fix: final commit on ci/cd fixes

--- rayanesh_bot/core/scripts/01_set_telegram_webhook_url.py
+++ rayanesh_bot/core/scripts/01_set_telegram_webhook_url.py
@@ -1,7 +1,7 @@
 import requests
 from django.conf import settings
 
-WEBHOOK_URL = f'http://{settings.DOMAIN}/api/bot/webhook/'
+WEBHOOK_URL = f'http://{settings.DOMAIN}:8000/api/bot/webhook/'
 SET_WEBHOOK_URL = f'https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/setWebhook?url={WEBHOOK_URL}'
 
 print('Requesting...')
