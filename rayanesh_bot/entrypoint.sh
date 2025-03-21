f829950b9a2be41e12a261ccd6772cb87a236fdd
Mehrad Milanloo <m.milanoo@bitpin.ir>
2025-03-21 21:30:37 +0330

    fix: changed deploy pipeline

--- rayanesh_bot/entrypoint.sh
+++ rayanesh_bot/entrypoint.sh
@@ -2,4 +2,11 @@
 
 set -e
 
+echo "Running migrations..."
+python manage.py makemigrations
+python manage.py migrate
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
 exec "$@"
